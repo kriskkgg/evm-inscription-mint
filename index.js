7aad93ec1e59150228189557a4c3e5b9736de460
kriskkgg <38906298+kriskkgg@users.noreply.github.com>
2023-12-20 22:54:00 +0800

    优化代码

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ethers } = require("ethers");
-const config = require("./config")
+const config = require("./config");
 
 // 连接到结点
 const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
@@ -8,19 +8,19 @@ const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
 const wallet = new ethers.Wallet(config.privateKey.trim(), provider);
 
 async function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // 转成16进制
-const convertToHexa = (str = '') =>{
-   const res = [];
-   const { length: len } = str;
-   for (let n = 0, l = len; n < l; n ++) {
-      const hex = Number(str.charCodeAt(n)).toString(16);
-      res.push(hex);
-   };
-   return `0x${res.join('')}`;
-}
+const convertToHexa = (str = "") => {
+  const res = [];
+  const { length: len } = str;
+  for (let n = 0, l = len; n < l; n++) {
+    const hex = Number(str.charCodeAt(n)).toString(16);
+    res.push(hex);
+  }
+  return `0x${res.join("")}`;
+};
 
 // 获取当前账户的 nonce
 async function getCurrentNonce(wallet) {
@@ -51,32 +51,41 @@ async function getGasLimit(hexData, address) {
   return gasLimit.toNumber();
 }
 
-// 转账交易
-async function sendTransaction(nonce) {
-  let hexData = config.tokenDataHex
-  if (config.tokenJson !== '') {
-    hexData	= convertToHexa(config.tokenJson.trim());
+function showBigNumber(bigNumberValue) {
+  try {
+    // 使用toString方法将BigNumber转换为gas wei单位的字符串
+    const w = ethers.utils.formatUnits(bigNumberValue, "wei"); // 'wei' 表示我们想要格式化为最小单位
+    return w / 100000000;
+  } catch (error) {
+    console.error("Error converting BigNumber to gas wei:", error);
+    return "Conversion failed";
   }
-  // 获取实时 gasPrice
-  const currentGasPrice = await getGasPrice();
-  // 在当前 gasPrice 上增加 一定倍数
-  const gasMultiple = parseInt(String(config.increaseGas * 100))
-  const increasedGasPrice = currentGasPrice.div(100).mul(gasMultiple);
-  // 获取钱包地址
-  let address = await wallet.getAddress();
-  if (config.receiveAddress !== "") {
-    address = config.receiveAddress;
-  }
-  // 获取当前 gasLimit 限制
-  const gasLimit = await getGasLimit(hexData, address);
-  // 付费金额
-  const payPrice = config.payPrice
+}
 
-  console.log("正在打铭文数据的16进制数据", hexData)
+// 转账交易
+async function sendTransaction(
+  nonce,
+  currentGasPrice,
+  gasLimit,
+  address,
+  hexData
+) {
+  // 获取实时 gasPrice
+  // 在当前 gasPrice 上增加 一定倍数
+  const gasMultiple = parseInt(String(config.increaseGas * 100));
+  const increasedGasPrice = currentGasPrice.div(100).mul(gasMultiple);
+
+  // 付费金额
+  const payPrice = config.payPrice;
+  console.log(
+    "正在打铭文数据的16进制数据",
+    hexData,
+    "\n铭文:" + config.tokenJson
+  );
 
   const transaction = {
     to: address,
-	// 替换为你要转账的金额
+    // 替换为你要转账的金额
     value: ethers.utils.parseEther(payPrice),
     // 十六进制数据
     data: hexData,
@@ -84,11 +93,12 @@ async function sendTransaction(nonce) {
     nonce: nonce,
     // 设置 gas 价格
     gasPrice: increasedGasPrice,
-	// 限制gasLimit，根据当前网络转账的设置，不知道设置多少的去区块浏览器看别人转账成功的是多少
+    // 限制gasLimit，根据当前网络转账的设置，不知道设置多少的去区块浏览器看别人转账成功的是多少
     gasLimit: gasLimit,
   };
 
   try {
+    console.log("Start Send. GasPrice", showBigNumber(increasedGasPrice));
     const tx = await wallet.sendTransaction(transaction);
     console.log(`Transaction with nonce ${nonce} hash:`, tx.hash);
   } catch (error) {
@@ -99,12 +109,38 @@ async function sendTransaction(nonce) {
 // 发送多次交易
 async function sendTransactions() {
   const currentNonce = await getCurrentNonce(wallet);
-  const sleepTime = config.sleepTime
+  const sleepTime = config.sleepTime;
+
+  // 获取钱包地址
+  let address = await wallet.getAddress();
+  if (config.receiveAddress !== "") {
+    address = config.receiveAddress;
+  }
+
+  let hexData = config.tokenDataHex;
+  if (config.tokenJson !== "") {
+    hexData = convertToHexa(config.tokenJson.trim());
+  }
+
+  let gasPrice = 0;
+  let gasLimit = 0;
+  gasLimit = await getGasLimit(hexData, address);
 
   for (let i = 0; i < config.repeatCount; i++) {
-    //const gasPrice = await getGasPrice();
-    await sendTransaction(currentNonce + i);
-    await sleep(sleepTime)
+    console.log(`第${i + 1}次`);
+    if (i % 10 == 0) {
+      gasPrice = await getGasPrice();
+      console.log("获取最新gas", showBigNumber(gasPrice), "gasLimit", gasLimit);
+    }
+
+    await sendTransaction(
+      currentNonce + i,
+      gasPrice,
+      gasLimit,
+      address,
+      hexData
+    );
+    await sleep(sleepTime);
   }
 }
 
